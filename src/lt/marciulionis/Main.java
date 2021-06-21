a702727d226aeb3e55e5f7bac800ea6fd1a87145
mindaugasmarc <mindaugaz.marc@gmail.com>
2021-06-21 14:15:08 +0300

    Calculate distance

---
+++ src/lt/marciulionis/Main.java
@@ -0,0 +1,28 @@
+package lt.marciulionis;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x1 = 5;
+        int y1 = 15;
+
+        int x2 = 10;
+        int y2 = 20;
+
+        double result = getResult(x1, y1, x2, y2);
+
+        System.out.println(result);
+
+
+
+    }
+
+    private static double getResult(int x1, int y1, int x2, int y2) {
+        double xDiffSq = Math.pow((x2 - x1), 2);
+        double yDiffSq = Math.pow((y2 - y1), 2);
+        double result = Math.sqrt(xDiffSq + yDiffSq);
+        return result;
+    }
+
+}
